f2c080241e90154c954b65405bac199a3de15cc9
SmolinR <rslnsmln@gmail.com>
2022-04-30 18:09:50 +0300

    Added cors library, added frontend url and PORT in constants

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const { Router } = require('express');
 const express = require('express');
+const cors = require('cors');
 const authRouter = require('./routers/auth_router')
 const bookRouter = require('./routers/books_router')
 const userRouter = require('./routers/user_router')
 const usersRouter = require('./routers/users_router');
 const categoriesRouter = require('./routers/categories_router')
-const res = require('express/lib/response');
-const { use } = require('express/lib/router');
 const mongoose = require('mongoose')
 const { MONGO_URL } = require('./constants');
-const categories = require('./models/categories');
+const { FRONTEND_URL } = require('./constants')
 mongoose
     .connect(MONGO_URL, { useNewUrlParser: true })
     .then(() => {
         const app = express()
+        app.use(cors({
+            origin: FRONTEND_URL
+        }))
         app.use(express.json())
         app.use('/auth', authRouter)
         app.use('/users', usersRouter)
