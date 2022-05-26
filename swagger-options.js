45101cccb2a1063a0a7869783ece9b6ef5e479e2
SmolinR <rslnsmln@gmail.com>
2022-05-26 18:05:49 +0300

    .

--- swagger-options.js
+++ swagger-options.js
@@ -32,9 +32,6 @@ const options = {
             url: 'http://localhost:3000',
         }],
     },
-    apis: ['./api/user/router.js', './api/user/model.js',
-        './api/book/router.js', './api/book/model.js',
-        './api/auth/router.js', './api/category/model.js',
-        './api/category/router.js', './api/admin/router.js'],
+    apis: ['./api/**/*.js'],
 };
 module.exports = options;
